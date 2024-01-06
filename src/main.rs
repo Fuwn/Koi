88f447de3131ff2dd5573646604228662e16b03c
Fuwn <contact@fuwn.me>
2024-01-06 06:39:10 -0800

    feat(main): koix variable for make-like on alias

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,38 @@ use itertools::Itertools;
 
 use crate::lexer::new as new_lexer;
 
-mod token;
-mod lexer;
 mod ast;
-mod parser;
 mod interp;
+mod lexer;
+mod parser;
+mod token;
 
 fn split_args() -> (Vec<String>, Vec<String>) {
-    let args = env::args().collect_vec();
+    if std::env::var("KOIX").is_ok() {
+        let args: Vec<String> = env::args().skip(1).collect();
+        let mut input = Vec::new();
+        let mut flags = Vec::new();
+        let mut after_separator = false;
 
-    if let Some(i) = args.iter().position(|arg| arg == "--") {
-        (args[..i].to_vec(), args[i + 1..].to_vec())
+        for arg in args {
+            if arg == "--" {
+                after_separator = true;
+            } else if after_separator {
+                flags.push(arg);
+            } else {
+                input.push(arg);
+            }
+        }
+
+        (flags, input)
     } else {
-        (args, vec![])
+        let args = env::args().collect_vec();
+
+        if let Some(i) = args.iter().position(|arg| arg == "--") {
+            (args[..i].to_vec(), args[i + 1..].to_vec())
+        } else {
+            (args, vec![])
+        }
     }
 }
 
@@ -38,7 +57,7 @@ fn main() {
                 .value_name("PATH")
                 .index(1)
                 .takes_value(true)
-                .help("Path to source file.")
+                .help("Path to source file."),
         )
         .arg(
             Arg::with_name("stdin")
@@ -46,14 +65,14 @@ fn main() {
                 .long("stdin")
                 .takes_value(false)
                 .help("Read script from stdin.")
-                .conflicts_with("path")
+                .conflicts_with("path"),
         )
         .arg(
             Arg::with_name("fn")
                 .short("f")
                 .long("--fn")
                 .takes_value(true)
-                .help("Function to call.")
+                .help("Function to call."),
         )
         .get_matches_from(koi_args);
 
@@ -74,21 +93,19 @@ fn main() {
     let mut interpreter = interp::Interpreter::new();
     interpreter.set_args(script_args);
     if let Some(path) = matches.value_of("path") {
-        let mut import_root = std::fs::canonicalize(PathBuf::from(path))
-            .expect("couldn't set import root");
+        let mut import_root =
+            std::fs::canonicalize(PathBuf::from(path)).expect("couldn't set import root");
         import_root.pop();
         interpreter.set_import_root(import_root);
     }
     interpreter.run(prog);
 
     if let Some(f) = matches.value_of("fn") {
-        use ast::{Stmt, Expr};
+        use ast::{Expr, Stmt};
 
-        interpreter.run(vec![
-            Stmt::Expr(Expr::Call {
-                func: Box::new(Expr::Get(f.to_string())),
-                args: vec![],
-            })
-        ]);
+        interpreter.run(vec![Stmt::Expr(Expr::Call {
+            func: Box::new(Expr::Get(f.to_string())),
+            args: vec![],
+        })]);
     }
 }
